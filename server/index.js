5a7b8e9a51efa10a57df27c253383aeb87bee254
AleCalz <calzadaloyaalejandra@gmail.com>
2024-09-03 19:14:49 -0600

    add websocket

--- server/index.js
+++ server/index.js
@@ -1,12 +1,33 @@
 import express from 'express'
 import logger from 'morgan'
 
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
 const port = process.env.PORT || 3000
 const app = express()
 
+//creamos nuestro server para poder utilizar todas las funciones de socket.io
+const server = createServer(app)
+//in out = io
+const io = new Server(server)
+//cuando el io tenga una conexion se ejecutará este callback
+io.on('connection', (socket) => {
+  console.log('user conected!')
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected!')
+  })
+
+  //cuando un socket de una especifica conexion reciba el evento chatMessage ejecuta el callback
+  socket.on('chatMessage', (message) => {
+    console.log(message)
+    io.emit('chatMessage', message)
+  })
+})
+
 //morgan es libreria que funciona a nivel de express para registrar las peticiones(logger)
 app.use(logger('dev')) //logger que se utilizara para modo desarrollo
-
 
 app.get('/', (req, res) => {
   //sendFile es una funcion de express que se utiliza para servir archivos, en este caso el index.html
@@ -14,9 +35,6 @@ app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/client/index.html')
 })
 
-
-
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
